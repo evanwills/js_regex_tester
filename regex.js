a60b40c282c9edf696b1c0be23cee0a8e5896550
Evan Wills <evan.wills@acu.edu.au>
2014-09-30 06:03:12 +1000

    updated regex.js Added the basic function skeleton based on jQuery style

--- regex.js
+++ regex.js
@@ -1,4 +1,36 @@
 "use strict";
-$('document').ready(function(){
 
+function checkModifiersOk() {
+
+}
+
+function textareaClick() {
+
+}
+
+function testRegex() {
+
+}
+
+function setActiveTab( whichTab ) {
+
+}
+
+function showOutput() {
+
+}
+
+$('document').ready(function(){
+	$('#regexp0 .checkbox-check :text').blur( checkModifiersOk() );
+
+	$('#regexp0 .checkbox-check :checkbox').click( textareaClick() );
+
+	$('#submitTest').click(function(){
+		$('#sample').testRegex();
+		setActiveTab(2);
+	});
+
+	$('#submitReplace').click(function(){
+		$('#sample').testRegex().showOutput();
+	});
 });
