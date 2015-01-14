f348662f67fe72ef597ce8073fde7654951c872a
Evan Wills <evan.wills@acu.edu.au>
2015-01-15 07:31:13 +1100

    updated regex_controller.inc.php; renamed regex_controller.inc.php to regex_check_controller.inc.php; Added a check to see if sample is split. If so, use the _multi view; changed the controller name to fit with the naming convention of the other classes.

--- regex_check_controller.inc.php
+++ regex_check_controller.inc.php
@@ -21,13 +21,17 @@ require_once('regex_check_child_'.$output_type.'.view.class.php');
 
 // ==================================================================
 
-debug('post');
 
 $model = new regex_check_parent_model();
 $classname = 'regex_check_child_view_'.$output_type;
 $child_view = new $classname($model);
 
 $classname = 'regex_check_parent_view_'.$output_type;
+if( $model->get_prop('split_sample') )
+{
+	$classname .= '_multi';
+}
+
 $parent_view = new $classname($model,$child_view);
 
 echo $parent_view->get_output();
