d7f425172a91902d5b1668e498cab3fb6be5d6ae
Evan Wills <evan.wills@acu.edu.au>
2015-01-15 07:40:47 +1100

    updated regex_check_parent.model.class.php; Removed default sample string. Added properties and functionality for sample splitting. Added functionality for trimming white space from output. Removed all get_ methods in favour of using the generic get_prop() method to access any of the private/protected methods.

--- regex_check_parent.model.class.php
+++ regex_check_parent.model.class.php
@@ -7,7 +7,9 @@ class regex_check_parent_model
 	protected $sample = '';
 	protected $ws_trim = false;
 	protected $ws_action_after = true;
-	protected $regex = array();
+	protected $split_sample = false;
+	protected $split_delim = '\n';
+	protected $regexes = array();
 	protected $output = '';
 	protected $output_is_different = false;
 	protected $test_only = true;
@@ -20,15 +22,61 @@ class regex_check_parent_model
 		if( isset($_POST) )
 		{
 			$this->dummy = false;
-			$this->sample = isset($_POST['sample'])?$_POST['sample']:"This is a line of text\nThis is another line\nand this is not the last line\nbut the last line will be empty.";
+			$this->sample = isset($_POST['sample'])?$_POST['sample']:'';
 			$this->ws_trim = isset($_POST['ws_trim'])?true:false;
 			if( isset($_POST['ws_action']) && $_POST['ws_action'] == 'before' )
 			{
 				$this->ws_action_after = false;
 			}
+			if( $this->ws_trim === true && $this->ws_action_after === false )
+			{
+				$this->sample = trim($this->sample);
+			}
+
+			$this->split_sample = isset($_POST['split_sample'])?true:false;
+			$this->split_delim = isset($_POST['split_delim'])?$_POST['split_delim']:'\n';
+
 			if( isset( $_POST['regex'] ) && is_array($_POST['regex']) && !empty($_POST['regex']) )
 			{
-				$output = $this->sample;
+				if( $this->ws_trim === true && $this->ws_action_after === false )
+				{
+					$output = $tmp_output = trim($this->sample);
+				}
+				else
+				{
+					$output = $tmp_output = $this->sample;
+				}
+
+				if( $this->split_sample === true )
+				{
+					$imploder = $this->split_delim;
+					switch($imploder)
+					{
+						case '\n':
+							$imploder = "\n";
+							break;
+						case '\r':
+							$imploder = "\r";
+							break;
+						case '\r\n':
+							$imploder = "\r\n";
+							break;
+						case '\t':
+							$imploder = "\t";
+							break;
+						case '\f':
+							$imploder = "\f";
+							break;
+						case '\e':
+							$imploder = "\e";
+							break;
+					}
+					$output = explode( $imploder , $output );
+				}
+				else
+				{
+					$output = array($output);
+				}
 				for( $a = 0 ; $a < count($_POST['regex']) ; $a += 1 )
 				{
 					$find = isset($_POST['regex'][$a]['find'])?$_POST['regex'][$a]['find']:'';
@@ -36,18 +84,33 @@ class regex_check_parent_model
 					$modifiers = isset($_POST['regex'][$a]['modifiers'])?$_POST['regex'][$a]['modifiers']:'';
 					$makeTextarea = isset($_POST['regex'][$a]['makeTextarea'])?true:false;
 					$tmp = new regex_check_child_model( $find , $replace , $modifiers , $makeTextarea );
-					$output = $tmp->process($output);
-					$this->regex[] = $tmp;
+					$this->regexes[] = $tmp;
+					for( $b = 0 ; $b < count($output) ; $b +=1 )
+					{
+						$output[$b] = $tmp->process($output[$b]);
+					}
 				}
-				if( $this->sample !== $output )
+				if( $this->split_sample === true )
 				{
+						$output = implode( $imploder , $output );
+				}
+				else
+				{
+					$output = $output[0];
+				}
+				if( $tmp_output !== $output )
+				{
+					if( $this->ws_trim === true && $this->ws_action_after === true )
+					{
+						$output = trim($output);
+					}
 					$this->output = $output;
 					$this->output_is_different = true;
 				}
 			}
 			else
 			{
-				$this->regex[] = new regex_check_child_model('','','',false,false);
+				$this->regexes[] = new regex_check_child_model('','','',false);
 			}
 			if( isset($_POST['submit_replace']) )
 			{
@@ -57,34 +120,17 @@ class regex_check_parent_model
 		}
 	}
 
-
-	public function get_regexes()
+	public function get_prop( $prop_name )
 	{
-		return $this->regex;
+		if( is_string($prop_name) && $prop_name != '' && property_exists($this,$prop_name) )
+		{
+			return $this->$prop_name;
+		}
+		else
+		{
+			// throw
+		}
 	}
 
-	public function is_dummy()
-	{
-		return $this->dummy;
-	}
 
-	public function is_test_only()
-	{
-		return $this->test_only;
-	}
-
-	public function is_output_different()
-	{
-		return $this->output_is_different;
-	}
-
-	public function get_output()
-	{
-		return $this->output;
-	}
-
-	public function get_uri()
-	{
-		return $this->request_uri;
-	}
 }
